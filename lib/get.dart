83e02d06a1a404220c51e76b8efa51d2e323b199
John Liton Mardy <mardy@ba-systems.com>
2024-03-07 00:09:13 +0600

    First commit

---
+++ lib/get.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class GetxScreen extends StatelessWidget {
+  const GetxScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
